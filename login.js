5f7338732698cc122ef31000effcb3809b8610b4
stutz2 <charliestutz12@gmail.com>
2020-11-15 16:45:26 -0500

    login code fragments

--- login.js
+++ login.js
@@ -12,13 +12,8 @@ function getMatches(){
     var userinfo=username+" "password,
     return;
     console.log(username+" "+password);
-    $.ajax({
-	     url: '/cgi-bin/stutz2_artproject.cgi?userinfo='+userinfo,
-	     type: "get",
-	     dataType: 'text',
-	     success: processResults,
-       error: function(){alert("Error: Something went wrong");}
-    });
+    openSQL();
+
 }
 
 processResults(results) {
@@ -28,3 +23,35 @@ processResults(results) {
     $(location).attr("href","http://jimskon.com/class/softdev/stutz2/ArtApp/art.html")
   }
 }
+
+
+var express = require('express');
+var app = express();
+var fs = require("fs");
+var mysql = require('mysql');
+pp.get('/', function(req, res)){
+  res.sendfile(
+})
+function openSQL() {
+  var con mysql.creatConnection({
+    host:"localhost",
+    user:"stutz2",
+    password:"S219536",
+    database:"stutz2"
+  });
+  con.connect(function(err) {
+    if(err) throw err;
+  });
+  return con;
+}
+
+var con = openSQL();
+
+app.get('/list', function (req, res) {
+  query="SELECT * FROM Userinfo"
+  con.query(query, function(err,result, fields){
+    if(err) throw err;
+    console.log(result)
+    res.end(JSON.stringify(result));
+  })
+})
