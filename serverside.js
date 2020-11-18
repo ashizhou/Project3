dc6d874b866744df4af9ed8d90bc2959d91f08d3
stutz12 <71220547+stutz12@users.noreply.github.com>
2020-11-17 21:13:32 -0500

    Create serverside.js

---
+++ serverside.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+var myseql = require('mysql');
+var port = 9016
+app.use(express.static('public'));
+app.get('/', function, req, res) {
+  res.sendFile(__dirname + "art.html");
+})
+
+function openSQL() {
+  var con mysql.createconnection ({
+    host:"localhost",
+    user: "stutz2",
+    password: "S219536",
+    database:"TeamB",
+  });
+  con.connect(function(err) {
+    if(err) throw err;
+  })
+  return con;
+}
+var con = openSQL();
+
+app.get('/list', function(req,res) {
+  query = "SELECT * FROM Userinfo";
+  con.query(query, function(err, result, fields) {
+    if(err) throw err;
+    console.log(result)
+    res.end( JSON.stringify(result));
+  })
+})
