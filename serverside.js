682e3383226e7ae55a8cabad7ce1913b541cd125
Jimmy <clark1@kenyon.edu>
2020-11-18 20:55:32 -0500

    Added find art function

--- serverside.js
+++ serverside.js
@@ -17,7 +17,7 @@ function openSQL() {
   });
   con.connect(function(err) {
     if(err) throw err;
-  })
+  });
   return con;
 }
 var con = openSQL();
@@ -30,6 +30,29 @@ app.get('/list', function(req,res) {
     res.end( JSON.stringify(result));
   })
 })
+
+app.get('/find', function (req, res) {
+    // find art
+    console.log("Query:"+JSON.stringify(req.query));
+    if (req.query.field === undefined || req.query.search === undefined) {
+    	console.log("Missing query value!");
+    	res.end('[]');
+    } else {
+    	field=req.query.field;
+    	search=req.query.search;
+    	console.log(field+":"+search);
+
+	query = "SELECT * FROM art WHERE "+field + "  like '%"+req.query.search+"%'";
+	console.log(query);
+	con.query(query, function(err,result,fields) {
+	    if (err) throw err;
+	    console.log(result)
+	    res.end( JSON.stringify(result));
+	})
+    }
+})
+
+
 var server = app.listen(port, function () {
    var host = server.address().address
    var port = server.address().port
