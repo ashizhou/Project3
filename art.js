bdb1d71041cef4bbccb6c078d29683dc4e9b375e
stutz2 <charliestutz12@gmail.com>
2020-11-18 21:01:39 -0500

    Art.js update

--- art.js
+++ art.js
@@ -1,33 +1,54 @@
-const port='9016'
+const port='9019'
 const Url = "http://jimskon.com"+port
 var operation;
 var selectID;
 var recIndex;
 var rows;
+var webPage="Login";
 var sqlTable ="";
 var saveRecord;
 
 $(document).ready(function () {
-  $(".modal").show();
   $(".btn-login").click( {
     sqlTable="userinfo.sql"
   });
-  $(".btn-login").click(getMatches)
+  $("#art-search").click(getMatches);
+  changeoperation(operation)
+  $(".btn-login").click(getMatches);
 
   $(".btn-createaccount").click(makeAccount);
 });
 
 function getMatches() {
   console.log("GetMatches:"+sqlTable);
-  var username = $("#username-login").val();
-  if($("#password-login").val()!="")
-  var password = $("#password-login").val();
+  if(sqlTable=="User Info") {
+    if("#usernaem-login").length()>1{
+      var username = $("#username-login").val();
+      var password = $("#password-login").val();
+      var search = username+" "+password; }
+    else
+      var search = $("#user-search-input").val();
+  }
+  else if(sqlTable=="Art") {
+    var search = $("#art-search-input").val();
+  }
   $.ajax({
-    url: Url+'/find?field='sqlTable+'&username='+username+'&password='+password,
+    url: Url+'/find?field='sqlTable+'&search'=search;
     type:"GET",
     success: processResults,
     error:displayError
   })
+}
+function pageChange(webPage) {
+/*  if(webPage=="Login") {
+    //login page functions will show
+}*/
+  if(webPage=="Main") {
+    //main page functions wiill show
+  }
+  if(webPage=="ArtSeach") {
+    //Art Results pages will show
+  }
 }
 function errorText() {
   $("#modal-login-text").hide();
