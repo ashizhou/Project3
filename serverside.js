f95bca2cec9dc65ef1970affc2f654d904cbbac8
stutz12 <71220547+stutz12@users.noreply.github.com>
2020-11-18 20:19:08 -0500

    Update serverside.js

--- serverside.js
+++ serverside.js
@@ -30,3 +30,8 @@ app.get('/list', function(req,res) {
     res.end( JSON.stringify(result));
   })
 })
+var server = app.listen(port, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
