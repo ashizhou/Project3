630ada0a69dfa99623278c5d8cdb71f3a17855a8
stutz2 <charliestutz12@gmail.com>
2020-11-29 11:38:19 -0500

    User info input work

--- art.js
+++ art.js
@@ -1,51 +1,67 @@
 const port='9019'
-const Url = "http://jimskon.com:"+port
+const Url ='http://jimskon.com:'+port//needed :
 var operation;
 var selectID;
 var recIndex;
 var rows;
 var webPage="Login";
-var sqlTable ="art.sql";
+var sqlTable ="";
 var saveRecord;
 
 $(document).ready(function () {
   /*$(".btn-login").click( {
     sqlTable="userinfo.sql"
   });*/
-  $("#art-search").click(getMatches);
- // changeoperation(operation);
- /* $(".btn-login").click(getMatches);
+    $(".modal").hide()
+    $(".user-profile").hide();
+    console.log("ready");
+    operation = "Artist";//default
+    $("#user-search").click(getList);
+
+//commented out bc we dont need it tomorrow i dont think
+    //console.log("ready");
+    //changeoperation(operation);
+  /*$(".btn-login").click(getMatches("Userinfo"));
 
   $(".btn-createaccount").click(makeAccount);
 */
+
+
   $(".dropdown-menu li a").click(function(){
     $(this).parents(".btn-group").find('.selection').text($(this).text());
     operation=$(this).text().split(" ").pop();  // Get last word
-    });
+    console.log(operation)
+  });
+  $("#art-search").click(getMatches);
+
 });
 
-function getMatches() {
-  console.log("GetMatches:"+sqlTable);
- /* if(sqlTable=="User Info") {
-    if("#usernaem-login").length()>1{
+function getMatches() {//if set on one function pass sqlTable to this
+  //basically gutted this for demo, we can implement later
+  //need a way to differentiate between tables, but cannot call in doc .ready with sql name or else wont search properly
+  //may be worth writing a seperate one for user/password
+
+  //for dual purpse:
+  /*if(sqlTable=="Userinfo") {
+    if("#username-login").length()>1{
       var username = $("#username-login").val();
       var password = $("#password-login").val();
       var search = username+" "+password; }
     else
       var search = $("#user-search-input").val();
   }
-  else if(sqlTable=="Art") {
-    var search = $("#art-search-input").val();
-  }*/
-  var search = $("#art-search-input").val();
+  else */
+  //if(sqlTable=="art") {
+    var search = $("#art-search-input").val();//sets search value
+    $('#art-search-input').empty();
+  //}
   $.ajax({
-    url: Url+'/find?field='sqlTable+'&search'=search;
+    url: Url+'/find?field='+operation+'&search='+search,//+'&sqlTable='+sqlTable, //is this how the find function works or should it be
     type:"GET",
     success: processResults,
     error:displayError
   })
 }
-
 
 function pageChange(webPage) {
 /*  if(webPage=="Login") {
@@ -62,33 +78,73 @@ function errorText() {
   $("#modal-login-text").hide();
   $("#modal-login-error-text").show();
 }
+ function getList(){
+   sqlTable="Userinfo.sql"
+   search=$("#user-search-input").val();
+   $.ajax({
+     url: Url+'/list?search='+search,
+     type:"GET",
+     success: processResults,
+     error:displayError
+   })
+   $('#results').append("this is working list");
+
+ }
+//outputs pictures after search
 function processResults(results) {
+  console.log(results);
   if(results=="") {
    errorText();
   }
-  else {
-    ('#modal').modal('hide');
-  }
-
-}
-
-function buildTable(data) {
-    rows=JSON.parse(data);
-    if (rows.length < 1) {
-	return "<h3>Nothing Found</h3>";
-    } else {
-	var result = '<table class="w3-table-all w3-hoverable" border="2"><tr><th>Title</th><th>Author</th><th>Form</th><th>Type</th><th>URL</th><tr>';
-	var i=0
-	rows.forEach(function(row) {
-	    result += "<tr><td class='title'>"+row.Title+"</td><td class='author'>"+row.Author+"</td><td class='form'>"+row.Form+"</td><td class='type'>"+row.Type+"</td><td class ='url'>"+row.URL+"</td>";
-	    i++;
-	})
-	result += "</table>";
-
-	return result;
+  else if(sqlTable=="Userinfo.sql") {
+    var userinfo = results.split(",");
+    for (var i=0; i<userinfo.length; i++) {
+      if(i == 0) {
+        userinfo[i] = userinfo[i].replace('[{','');
+      } else if (i == userinfo.length-1) {
+        userinfo[i] = userinfo[i].replace('}]','');
+      }
+      userinfo[i]=userinfo[i].split(":").pop();
+      for(var j=0; j<userinfo[i].length; j++) {
+        userinfo[i][j]=userinfo[i][j].replace('""','');
+      }
+      console.log(userinfo[i]);
+      userinfo[0]
     }
-}
+    var username = userinfo[0];
 
-function clearResults(){
-	$('#searchresults').empty();
+    $(".search-option").hide();
+    $(".user-profile").show();
+    $("#user-username").text(userinfo[0]);
+    $("#user-biography").text(userinfo[2]);
+    $(".favorite-artists").text(userinfo[3])
+
+  }
+  else if (sqlTable=="art.sql"){
+  $('#results').html("");//clears past searc results
+  var arrayOfPics = results.split(",");//seperated urls from results
+  //for loop for putting pictures on the screen
+  var i;
+  for(i = 0; i < arrayOfPics.length; i++){
+    //cleaning urls to become picture src
+  if (i == 0){
+    arrayOfPics[i]=arrayOfPics[i].replace('[', '');
+  }
+  if (i == arrayOfPics.length-1){
+    arrayOfPics[i]=arrayOfPics[i].replace(']', '');
+  }
+  var pic = arrayOfPics[i].replace('{"URL":"', '');
+  var pic2 = pic.replace('"}', '');
+
+  //puts picture on website
+  $(`<img src='${pic2}'>`).appendTo('#results');
+  }
+  }
+  else {
+    console.log(":(");
+  }
+}
+//shows error
+function displayError(error) {
+    console.log('Error ${error}');
 }
--- serverside.js
+++ serverside.js
@@ -1,9 +1,14 @@
+//
+//
 var express = require('express');
 var app = express();
 var fs = require("fs");
 var mysql = require('mysql');
+//
 var port = 9019
 app.use(express.static('public'));
+//
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + "/art.html");
 })
@@ -22,9 +27,11 @@ function openSQL() {
 }
 var con = openSQL();
 
-app.get('/list', function(req,res) {
-  query = "SELECT * FROM art";
-  con.query(query, function(err, result, fields) {
+app.get('/list', function (req,res) {
+  console.log("Query:"+JSON.stringify(req.query));
+  search=req.query.search;
+  query = "SELECT * FROM Userinfo WHERE Username ='"+search+"'";//sends back all results  from user row 
+  con.query(query, function(err, result, search) {
     if(err) throw err;
     console.log(result)
     res.end( JSON.stringify(result));
@@ -35,20 +42,21 @@ app.get('/find', function (req, res) {
     // find art
     console.log("Query:"+JSON.stringify(req.query));
     if (req.query.field === undefined || req.query.search === undefined) {
-    	console.log("Missing query value!");
-    	res.end('[]');
+        console.log("Missing query value!");
+        res.end('[]');
     } else {
-    	field=req.query.field;
-    	search=req.query.search;
-    	console.log(field+":"+search);
-
-	query = "SELECT * FROM art WHERE "+field + "  like '%"+req.query.search+"%'";
-	console.log(query);
-	con.query(query, function(err,result,fields) {
-	    if (err) throw err;
-	    console.log(result)
-	    res.end( JSON.stringify(result));
-	})
+        field=req.query.field;
+        search=req.query.search;
+      //sqlTable=req.query.sqlTable; //added to make usable for user and art
+        console.log(field+":"+search);
+  //if we're passing the SQl table as an argument, it needs to go here, otherwise we need 2 seperate functions
+        query = "SELECT URL FROM art WHERE "+field + "  like '%"+req.query.search+"%'";// LIMIT 20
+        console.log(query);
+        con.query(query, function(err,result,fields,sqlTable) {
+            if (err) throw err;
+            console.log(result)
+            res.end( JSON.stringify(result));
+        })
     }
 })
 
