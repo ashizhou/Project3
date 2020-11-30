61bffd33107b8724dd412e1f221f81fdb0ea1fdb
Jimmy <clark1@kenyon.edu>
2020-11-30 11:04:39 -0500

    Update github

--- serverside.js
+++ serverside.js
@@ -4,14 +4,12 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 var mysql = require('mysql');
-var pictur="https://www.wga.hu/detail/a/aachen/allegory.jpg";//this is temporary. Used to test create account
 //
 var port = 9019
 app.use(express.static('public'));
 //
-
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + "/public/" + "art.html");
+  res.sendFile(__dirname + "/art.html");
 })
 
 function openSQL() {
@@ -29,10 +27,9 @@ function openSQL() {
 var con = openSQL();
 
 app.get('/list', function (req,res) {
-  console.log("Query:"+JSON.stringify(req.query));
-  search=req.query.search;
-  query = "SELECT * FROM Userinfo WHERE Username ='"+search+"'";//sends back all results  from user row
-  con.query(query, function(err, result, search) {
+
+  query = "SELECT * FROM art";
+  con.query(query, function(err, result, fields) {
     if(err) throw err;
     console.log(result)
     res.end( JSON.stringify(result));
@@ -43,31 +40,16 @@ app.get('/find', function (req, res) {
     // find art
     console.log("Query:"+JSON.stringify(req.query));
     if (req.query.field === undefined || req.query.search === undefined) {
-        console.log("Missing query value!");
-        res.end('[]');
+    	console.log("Missing query value!");
+    	res.end('[]');
     } else {
-        field=req.query.field;
-        search=req.query.search;
+    	field=req.query.field;
+    	search=req.query.search;
       //sqlTable=req.query.sqlTable; //added to make usable for user and art
-        console.log(field+":"+search);
+    	console.log(field+":"+search);
   //if we're passing the SQl table as an argument, it needs to go here, otherwise we need 2 seperate functions
-        query = "SELECT URL FROM art WHERE "+field + "  like '%"+req.query.search+"%'";// LIMIT 20
-        console.log(query);
-        con.query(query, function(err,result,fields,sqlTable) {
-            if (err) throw err;
-            console.log(result)
-            res.end( JSON.stringify(result));
-        })
-    }
-})
-app.get('/addrec', function (req, res) {
-    if (missingField(req.query)) {
-        console.log("Bad add request:"+JSON.stringify(req.query));
-        res.end("['fail']");
-    } else {
-    console.log('Hello');
-	query = "Insert INTO Userinfo(Username, Password, Biography, FavoriteArtists, Picture)  VALUES('"+req.query.Username+"','"+req.query.Password+"','"+req.query.Biography+"','"+req.query.FavoriteArtists+"','"+req.query.Picture+"')";
- 	console.log(query);
+	query = "SELECT * FROM art WHERE "+field + "  like '%"+req.query.search+"%' LIMIT 50";
+	console.log(query);
 	con.query(query, function(err,result,fields) {
 	    if (err) throw err;
 	    console.log(result)
@@ -75,9 +57,6 @@ app.get('/addrec', function (req, res) {
 	})
     }
 })
-function missingField(p) {
-    return (p.Username === undefined || p.Password === undefined || p.Biography === undefined || p.FavoriteArtists == undefined || p.Picture == undefined);
-}
 
 
 var server = app.listen(port, function () {
